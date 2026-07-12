88880b7c2447f77d53932de7ab784195f2c5e512
Syed Naqvi <129780232+5yed@users.noreply.github.com>
2026-07-12 18:13:15 +0100

    Update ignore.lua

--- ignore.lua
+++ ignore.lua
@@ -306,9 +306,9 @@ local Group1Set = {
 -----------------------------------------------
 -- CAR TELEPORT SYSTEM
 -----------------------------------------------
-local STEP_SIZE = 1000
-local HEIGHT_ABOVE_GROUND = 8
-local WAIT_BETWEEN_JUMPS = 2
+local STEP_SIZE = 200
+local HEIGHT_ABOVE_GROUND = 2
+local WAIT_BETWEEN_JUMPS = 0.5
 
 local function FreezeVehicle(vehicle)
     for _, part in ipairs(vehicle:GetDescendants()) do
